453eda699fa10e136d3f2df6aade3201deca3efb
Jerry W <jerrywiltse@gmail.com>
2017-08-21 11:22:13 -0400

    working 1.62.0

--- conan-boost-errors/src/mytest.cpp
+++ conan-boost-errors/src/mytest.cpp
@@ -1,7 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
 #include <boost/program_options.hpp>
 
 int main(int argc, char * argv[])
 {
+  namespace bpo = boost::program_options;
   bpo::options_description desc;
   bpo::variables_map vm;
 
@@ -11,12 +15,8 @@ int main(int argc, char * argv[])
   bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
   bpo::notify(vm);
   
-  std::string testvalue;
-  if (vm.count("testarg"))
-  {
-    testvalue = vm["testarg"].as<std::string>();
-  }
-
-  std::cout << "You passed in argument `testarg` with value: " << testvalue;
+  std::cout << "This runs without conan imports with Boost/1.62.0";
+  std::string input;
+  getline(std::cin, input);
   return 0;
 }
